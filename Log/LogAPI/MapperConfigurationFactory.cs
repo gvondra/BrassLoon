57fa04680864152e7a834f9702960cc88ece3d25
Greg Vondra <gvondra@gmail.com>
2020-11-29 10:14:26 -0600

    add mapper configuration

---
+++ Log/LogAPI/MapperConfigurationFactory.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using BrassLoon.Interface.Log.Models;
+using BrassLoon.Log.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogAPI
+{
+    public class MapperConfigurationFactory
+    {
+        private static MapperConfiguration _mapperConfiguratin;
+
+        static MapperConfigurationFactory()
+        {
+            _mapperConfiguratin = new MapperConfiguration(config =>
+            {
+                config.CreateMap<Trace, ITrace>();
+                config.CreateMap<ITrace, Trace>();
+            });
+        }
+
+        public static Mapper CreateMapper()
+        {
+            return new Mapper(_mapperConfiguratin);
+        }
+    }
+}
