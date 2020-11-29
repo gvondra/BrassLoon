48cd508206ed8f3055192301de23c07104ca7aeb
Greg Vondra <gvondra@gmail.com>
2020-11-29 10:14:43 -0600

    register di container

---
+++ Log/LogAPI/LogAPIModule.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogAPI
+{
+    public class LogAPIModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+            builder.RegisterInstance(new SettingsFactory());
+            builder.RegisterModule(new BrassLoon.Log.Core.LogModule());
+        }
+    }
+
+    public static class LogAPIModuleExtensions
+    {
+        public static IServiceCollection AddDiContainer(this IServiceCollection services)
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterModule(new LogAPIModule());
+            services.AddSingleton<IContainer>(builder.Build());
+            return services;
+        }
+    }
+}
--- Log/LogAPI/Startup.cs
+++ Log/LogAPI/Startup.cs
@@ -25,6 +25,8 @@ namespace LogAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<Settings>(Configuration);
+            services.AddDiContainer();
             services.AddControllers()
                 .AddJsonOptions(o =>
                 {
