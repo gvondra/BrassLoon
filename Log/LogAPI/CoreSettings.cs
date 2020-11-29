988ad0288a56606f64f045980a4e3a4db52a3dad
Greg Vondra <gvondra@gmail.com>
2020-11-29 10:14:15 -0600

    add settings

---
+++ Log/LogAPI/CoreSettings.cs
@@ -0,0 +1,13 @@
+﻿using BrassLoon.CommonCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogAPI
+{
+    public class CoreSettings : ISettings
+    {
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ Log/LogAPI/SettingsFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogAPI
+{
+    public class SettingsFactory
+    {
+        public CoreSettings CreateCore(Settings settings)
+        {
+            return new CoreSettings()
+            {
+                ConnectionString = settings.ConnectionString
+            };
+        }
+    }
+}
