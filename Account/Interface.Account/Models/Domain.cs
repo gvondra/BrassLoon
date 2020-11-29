0df5b41ff524cf816a140cd965dadcdeea8f1e22
Greg Vondra <gvondra@gmail.com>
2020-11-29 09:31:16 -0600

    add property setters

--- Account/Interface.Account/Models/Domain.cs
+++ Account/Interface.Account/Models/Domain.cs
@@ -6,10 +6,10 @@ namespace BrassLoon.Interface.Account.Models
 {
     public class Domain
     {
-        public Guid? DomainId { get; }
-        public Guid? AccountId { get; }
+        public Guid? DomainId { get; set; }
+        public Guid? AccountId { get; set; }
         public string Name { get; set; }
-        public DateTime? CreateTimestamp { get; }
-        public DateTime? UpdateTimestamp { get; }
+        public DateTime? CreateTimestamp { get; set; }
+        public DateTime? UpdateTimestamp { get; set; }
     }
 }
