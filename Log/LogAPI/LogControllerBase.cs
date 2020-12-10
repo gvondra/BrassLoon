4eb2399a62ac9642d115e990e6c11c71d9347428
Greg Vondra <gvondra@gmail.com>
2020-12-10 16:22:50 -0600

    Cache domain for auth check

--- Log/LogAPI/LogControllerBase.cs
+++ Log/LogAPI/LogControllerBase.cs
@@ -1,9 +1,16 @@
 ﻿using BrassLoon.Interface.Account;
 using BrassLoon.Interface.Account.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Polly;
+using Polly.Caching;
+using Polly.Caching.Memory;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -11,14 +18,34 @@ namespace LogAPI
 {
     public abstract class LogControllerBase : ControllerBase
     {
+        private static Policy m_cache = Policy.Cache(new MemoryCacheProvider(new MemoryCache(new MemoryCacheOptions())), new SlidingTtl(TimeSpan.FromSeconds(90)));
+
         [NonAction]
         protected async Task<bool> VerifyDomainAccount(Guid domainId, SettingsFactory settingsFactory, Settings settings, IDomainService domainService)
         {
-            Domain domain = await domainService.Get(
-                settingsFactory.CreateAccount(settings, GetAccessToken()),
-                domainId
+            Domain domain = await GetDomain(
+                domainId,
+                settingsFactory,
+                settings,
+                GetAccessToken(),
+                domainService
                 );
             return VerifyDomainAccount(domain);
+        }
+
+        private async Task<Domain> GetDomain(Guid domainId, SettingsFactory settingsFactory, Settings settings, string accessToken, IDomainService domainService)
+        {
+            HashAlgorithm hashAlgorithm = SHA256.Create();
+            string hash = Convert.ToBase64String(hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(accessToken)));
+            return await m_cache.Execute<Task<Domain>>(async context =>
+            {
+                return await domainService.Get(
+                settingsFactory.CreateAccount(settings, accessToken),
+                domainId
+                );
+            },
+            new Context(string.Format("{0:N}::{1}::{2}", domainId, hash, settings.AccountApiBaseAddress))
+            );
         }
 
         [NonAction]
