62907f355b2eb3c9ca8c735306e45855891e5895
Greg Vondra <gvondra@gmail.com>
2020-12-10 11:28:23 -0600

    Confirm logged in before getting accounts

--- BrassLoon/src/app/accounts/accounts.component.ts
+++ BrassLoon/src/app/accounts/accounts.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { AccountService } from '../services/account.service';
 import { Account } from '../models/account';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-accounts',
@@ -17,15 +18,25 @@ export class AccountsComponent implements OnInit {
   faPlus = faPlus;
 
   constructor(private accountService: AccountService,
-    private router: Router) { }
+    private router: Router,
+    private oidcSecurityService: OidcSecurityService) { }
 
   ngOnInit(): void {
+    this.oidcSecurityService.isAuthenticated$.subscribe(isAuthenticated => {
+      this.Accounts = null;
+      if (isAuthenticated) {
+        this.LoadAccounts();
+      }
+    });    
+  }
+
+  private LoadAccounts() {
     this.accountService.GetAll()
     .then(res => this.Accounts = res)
     .catch(err => {
       console.error(err);
       this.ErrorMessage = err.message || "Unexpected Error"
-    });
+    });    
   }
 
   AddAccount() {
