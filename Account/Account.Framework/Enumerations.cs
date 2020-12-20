858f3e33bcd867eb906ef272c7922ad43a905bf5
Greg Vondra <gvondra@gmail.com>
2020-12-20 10:10:02 -0600

    add user role enumeration

---
+++ Account/Account.Framework/Enumerations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrassLoon.Account.Framework.Enumerations
+{
+    [Flags()]
+    public enum UserRole : short
+    {
+        None = 0,
+        SystemAdministrator = 1
+    }
+}
