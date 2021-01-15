c30bbad3c0bab8956ac6f08f9263be12f10c5c57
Greg Vondra <gvondra@gmail.com>
2021-01-15 07:22:34 -0600

    add insert and update stored procedures

---
+++ Config/Database/Stored Procedures/blc.CreateItem.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [blc].[CreateItem]
+	@id UNIQUEIDENTIFIER OUT,
+	@domainId UNIQUEIDENTIFIER,
+	@code VARCHAR(256),
+	@data VARCHAR(MAX),
+	@timestamp DATETIME2(4) OUT
+AS
+BEGIN
+	SET @timestamp = SYSUTCDATETIME();
+	SET @id = NEWID();
+	INSERT INTO [blc].[Item] ([ItemId], [DomainId], [Code], [Data], [CreateTimestamp], [UpdateTimestamp]) 
+	VALUES (@id, @domainId, @code, @data, @timestamp, @timestamp) 
+	;
+END
---
+++ Config/Database/Stored Procedures/blc.CreateLookup.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [blc].[CreateLookup]
+	@id UNIQUEIDENTIFIER OUT,
+	@domainId UNIQUEIDENTIFIER,
+	@code VARCHAR(256),
+	@data VARCHAR(MAX),
+	@timestamp DATETIME2(4) OUT
+AS
+BEGIN
+	SET @timestamp = SYSUTCDATETIME();
+	SET @id = NEWID();
+	INSERT INTO [blc].[Lookup] ([LookupId], [DomainId], [Code], [Data], [CreateTimestamp], [UpdateTimestamp]) 
+	VALUES (@id, @domainId, @code, @data, @timestamp, @timestamp) 
+	;
+END
