3411174c1c8292fc9af0f035a1527a8f085e99fe
Greg Vondra <gvondra@gmail.com>
2021-01-15 16:36:47 -0600

    add controller base

--- Config/ConfigAPI/SettingsFactory.cs
+++ Config/ConfigAPI/SettingsFactory.cs
@@ -11,5 +11,21 @@ namespace ConfigAPI
         {
             return new CoreSettings(settings);
         }
+
+        public AccountSettings CreateAccount(Settings settings, string accessToken)
+        {
+            return new AccountSettings(accessToken)
+            {
+                BaseAddress = settings.AccountApiBaseAddress
+            };
+        }
+
+        public LogSettings CreateLog(Settings settings, string accessToken)
+        {
+            return new LogSettings(accessToken)
+            {
+                BaseAddress = settings.LogApiBaseAddress
+            };
+        }
     }
 }
