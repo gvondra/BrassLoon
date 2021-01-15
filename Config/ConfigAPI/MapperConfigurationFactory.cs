3c09444d13de07380199460c64b7469b3d5a71ee
Greg Vondra <gvondra@gmail.com>
2021-01-15 16:30:37 -0600

    add mapper config factory

---
+++ Config/ConfigAPI/MapperConfigurationFactory.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using BrassLoon.Interface.Config.Models;
+using BrassLoon.Config.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConfigAPI
+{
+    public class MapperConfigurationFactory
+    {
+        private static MapperConfiguration _mapperConfiguratin;
+
+        static MapperConfigurationFactory()
+        {
+            _mapperConfiguratin = new MapperConfiguration(config =>
+            {
+                config.CreateMap<Item, IItem>();
+                config.CreateMap<IItem, Item>();
+                config.CreateMap<IItemHistory, ItemHistory>();
+                config.CreateMap<Lookup, ILookup>();
+                config.CreateMap<ILookup, Lookup>();
+                config.CreateMap<ILookupHistory, LookupHistory>();
+            });
+        }
+
+        public static Mapper CreateMapper()
+        {
+            return new Mapper(_mapperConfiguratin);
+        }
+    }
+}
