ef11b1960b62b38143a375f05e5d1576c7d93388
Greg Vondra <gvondra@gmail.com>
2021-01-15 15:57:35 -0600

    add settings

---
+++ Config/ConfigAPI/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigAPI
+{
+    public class Settings
+    {
+        public string ConnectionString { get; set; }
+        public string ConnectionStringUser { get; set; }
+        public bool EnableDatabaseAccessToken { get; set; } = false;
+        public string KeyVaultAddress { get; set; }
+        public string AccountApiBaseAddress { get; set; }
+        public string LogApiBaseAddress { get; set; }
+        public string ExceptionLoggingDomainId { get; set; }
+    }
+}
--- Config/ConfigAPI/Startup.cs
+++ Config/ConfigAPI/Startup.cs
@@ -26,6 +26,7 @@ namespace ConfigAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<Settings>(Configuration);
             services.AddControllers()
                 .AddNewtonsoftJson(o =>
                 {
