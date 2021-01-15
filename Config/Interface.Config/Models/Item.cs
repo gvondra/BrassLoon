9102d6a0a9eb8ba67eadad572550bbdf63f7c6dc
Greg Vondra <gvondra@gmail.com>
2021-01-15 16:37:13 -0600

    add interface models

---
+++ Config/Interface.Config/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrassLoon.Interface.Config.Models
+{
+    public class Item
+    {
+        public Guid? ItemId { get; set; }
+        public Guid? DomainId { get; set; }
+        public string Code { get; set; }
+        public string Data { get; set; }
+        public DateTime? CreateTimestamp { get; set; }
+        public DateTime? UpdateTimestamp { get; set; }
+    }
+}
