e328f5b2edaa402aec77cebad9c01472ea715131
Greg Vondra <gvondra@gmail.com>
2021-01-17 08:38:02 -0600

    register data tier di module

--- Config/Config.Core/ConfigModule.cs
+++ Config/Config.Core/ConfigModule.cs
@@ -11,6 +11,7 @@ namespace BrassLoon.Config.Core
         protected override void Load(ContainerBuilder builder)
         {
             base.Load(builder);
+            builder.RegisterModule(new BrassLoon.Config.Data.ConfigDataModule());
             builder.RegisterInstance(new SettingsFactory());
             builder.RegisterType<ItemFactory>().As<IItemFactory>();
             builder.RegisterType<ItemSaver>().As<IItemSaver>();
