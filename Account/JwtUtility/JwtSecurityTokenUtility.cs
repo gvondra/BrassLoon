619d69773323abd89ab9719b5700bcb25982167a
Greg Vondra <gvondra@gmail.com>
2022-11-26 13:10:34 -0600

    add jwt utility project

---
+++ Account/JwtUtility/JwtSecurityTokenUtility.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+
+namespace BrassLoon.JwtUtility
+{
+    public static class JwtSecurityTokenUtility
+    {
+        public static string CreateJwtId() => Guid.NewGuid().ToString("N");
+
+        public static JwtSecurityToken Create(
+            string tknCsp,
+            string issuer,
+            string audience,
+            IEnumerable<Claim> claims,
+            Func<DateTime> expiration)
+        => Create(tknCsp, issuer, audience, claims, expiration, CreateJwtId);
+
+        public static JwtSecurityToken Create(
+            string tknCsp, 
+            string issuer,
+            string audience,
+            IEnumerable<Claim> claims,
+            Func<DateTime> expiration,
+            Func<string> jwtId)
+        {
+            RsaSecurityKey securityKey = RsaSecurityKeySerializer.GetSecurityKey(tknCsp, true);
+            SigningCredentials credentials = new SigningCredentials(securityKey, SecurityAlgorithms.RsaSha512);
+            if (claims == null)
+                claims = new List<Claim>();
+            claims =  new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, jwtId())
+            }
+            .Concat(claims);
+            return new JwtSecurityToken(
+                issuer,
+                audience,
+                claims,
+                expires: expiration(),
+                signingCredentials: credentials
+            );
+        }
+
+        public static string Write(JwtSecurityToken jwtSecurityToken) => new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+    }
+}
---
+++ Account/JwtUtility/RsaSecurityKeySerializer.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BrassLoon.JwtUtility
+{
+    public static class RsaSecurityKeySerializer
+    {
+        public static RsaSecurityKey GetSecurityKey(string tknCsp, bool includePublicKey = false)
+        {
+            dynamic tknCspData = JsonConvert.DeserializeObject(Encoding.UTF8.GetString(Convert.FromBase64String(tknCsp)));
+            RSAParameters rsaParameters = new RSAParameters
+            {
+                D = Base64UrlEncoder.DecodeBytes((string)tknCspData.d),
+                DP = Base64UrlEncoder.DecodeBytes((string)tknCspData.dp),
+                DQ = Base64UrlEncoder.DecodeBytes((string)tknCspData.dq),
+                Exponent = Base64UrlEncoder.DecodeBytes((string)tknCspData.exponent),
+                InverseQ = Base64UrlEncoder.DecodeBytes((string)tknCspData.inverseQ),
+                Modulus = Base64UrlEncoder.DecodeBytes((string)tknCspData.modulus),
+                P = Base64UrlEncoder.DecodeBytes((string)tknCspData.p),
+                Q = Base64UrlEncoder.DecodeBytes((string)tknCspData.q)
+            };
+#if (NETSTANDARD2_0)
+            RSA rsa = RSA.Create();
+            rsa.ImportParameters(rsaParameters);
+            return new RsaSecurityKey(rsa.ExportParameters(includePublicKey));
+#else
+            return new RsaSecurityKey(RSA.Create(rsaParameters).ExportParameters(includePublicKey));
+#endif
+        }
+    }
+}
