5b654a30d62a889be04fdeb04f6f17be4af6f137
Greg Vondra <gvondra@gmail.com>
2022-12-16 13:02:00 -0600

    add email address business tier

---
+++ Authorization/Authorization.Framework/IEmailAddress.cs
@@ -0,0 +1,20 @@
+﻿using BrassLoon.CommonCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrassLoon.Authorization.Framework
+{
+    public interface IEmailAddress
+    {
+        Guid EmailAddressId { get; }
+        string Address { get; }
+        byte[] AddressHash { get; }
+        DateTime CreateTimestamp { get; }
+
+        Task Create(ITransactionHandler transactionHandler);
+    }
+}
---
+++ Authorization/Authorization.Framework/IEmailAddressFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrassLoon.Authorization.Framework
+{
+    public interface IEmailAddressFactory
+    {
+        Task<IEmailAddress> Get(ISettings settings, Guid id);
+        Task<IEmailAddress> GetByAddress(ISettings settings, string address);
+    }
+}
