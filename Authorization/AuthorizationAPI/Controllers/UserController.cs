d87280e58852dd5a2bef0598702928c2a1f97b66
Greg Vondra <gvondra@gmail.com>
2022-12-17 11:10:05 -0600

    start controllers

---
+++ Authorization/AuthorizationAPI/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using BrassLoon.Interface.Account;
+using BrassLoon.Interface.Log;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace AuthorizationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : AuthorizationContollerBase
+    {
+        public UserController(IOptions<Settings> settings,
+            SettingsFactory settingsFactory,
+            IExceptionService exceptionService,
+            MapperFactory mapperFactory,
+            IDomainService domainService)
+            : base(settings, settingsFactory, exceptionService, mapperFactory, domainService)
+        { }
+    }
+}
