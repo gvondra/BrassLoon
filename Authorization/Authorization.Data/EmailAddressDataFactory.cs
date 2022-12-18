fd12740b616c836b761e492950c99be2fd9d18bd
Greg Vondra <gvondra@gmail.com>
2022-12-18 13:00:49 -0600

    add email address data factory and saver

---
+++ Authorization/Authorization.Data/EmailAddressDataFactory.cs
@@ -0,0 +1,47 @@
+﻿using BrassLoon.Authorization.Data.Framework;
+using BrassLoon.Authorization.Data.Models;
+using BrassLoon.DataClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrassLoon.Authorization.Data
+{
+    public class EmailAddressDataFactory : DataFactoryBase<EmailAddressData>, IEmailAddressDataFactory
+    {
+        public EmailAddressDataFactory(IDbProviderFactory providerFactory) : base(providerFactory) { }
+
+        public async Task<EmailAddressData> Get(ISqlSettings settings, Guid id)
+        {
+            IDataParameter[] parameters = new IDataParameter[]
+            {
+                DataUtil.CreateParameter(_providerFactory, "id", DbType.Guid, id)
+            };
+            return (await _genericDataFactory.GetData(settings,
+                _providerFactory,
+                "[blt].[GetEmailAddress]",
+                Create,
+                DataUtil.AssignDataStateManager,
+                parameters))
+                .FirstOrDefault();
+        }
+
+        public async Task<EmailAddressData> GetByAddressHash(ISqlSettings settings, byte[] hash)
+        {
+            IDataParameter[] parameters = new IDataParameter[]
+            {
+                DataUtil.CreateParameter(_providerFactory, "addressHash", DbType.Binary, hash)
+            };
+            return (await _genericDataFactory.GetData(settings,
+                _providerFactory,
+                "[blt].[GetEmailAddress_by_AddressHash]",
+                Create,
+                DataUtil.AssignDataStateManager,
+                parameters))
+                .FirstOrDefault();
+        }
+    }
+}
