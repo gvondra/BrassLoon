5e71870a43970e8a1be891ee1520f47c69e5a883
Greg Vondra <gvondra@gmail.com>
2022-12-18 11:10:44 -0600

    add create overload

--- Account/JwtUtility/JwtSecurityTokenUtility.cs
+++ Account/JwtUtility/JwtSecurityTokenUtility.cs
@@ -28,10 +28,21 @@ namespace BrassLoon.JwtUtility
             Func<string> jwtId)
         {
             RsaSecurityKey securityKey = RsaSecurityKeySerializer.GetSecurityKey(tknCsp, true);
+            return Create(securityKey, issuer, audience, claims, expiration, jwtId);
+        }
+
+        public static JwtSecurityToken Create(
+            RsaSecurityKey securityKey,
+            string issuer,
+            string audience,
+            IEnumerable<Claim> claims,
+            Func<DateTime> expiration,
+            Func<string> jwtId)
+        {
             SigningCredentials credentials = new SigningCredentials(securityKey, SecurityAlgorithms.RsaSha512);
             if (claims == null)
                 claims = new List<Claim>();
-            claims =  new List<Claim>
+            claims = new List<Claim>
             {
                 new Claim(JwtRegisteredClaimNames.Jti, jwtId())
             }
