5e9f7884a7c997f23007d9807aa3e40a6de1bce4
Greg Vondra <gvondra@gmail.com>
2022-12-27 15:38:57 -0600

    rename mapper factory

--- Log/LogAPI/LogAPIModule.cs
+++ Log/LogAPI/LogAPIModule.cs
@@ -6,7 +6,8 @@ namespace LogAPI
         protected override void Load(ContainerBuilder builder)
         {
             base.Load(builder);
-            builder.RegisterInstance(new SettingsFactory());
+            builder.RegisterType<MapperFactory>().SingleInstance();
+            builder.RegisterType<SettingsFactory>().SingleInstance();
             builder.RegisterModule(new BrassLoon.Log.Core.LogModule());
             builder.RegisterModule(new BrassLoon.Interface.Account.AccountInterfaceModule());
             builder.RegisterModule(new BrassLoon.Interface.Log.LogInterfaceModule());
---
+++ Log/LogAPI/MapperFactory.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using BrassLoon.Interface.Log.Models;
+using BrassLoon.Log.Framework;
+using BrassLoon.Log.Framework.Enumerations;
+using System;
+using System.Collections.Generic;
+
+namespace LogAPI
+{
+    public class MapperFactory
+    {
+        private static MapperConfiguration _mapperConfiguratin;
+
+        static MapperFactory()
+        {
+            _mapperConfiguratin = new MapperConfiguration(LoadConfiguration);
+        }
+
+        private static void LoadConfiguration(IMapperConfigurationExpression config)
+        {
+            config.CreateMap<BrassLoon.Interface.Log.Models.Exception, IException>();
+            config.CreateMap<IException, BrassLoon.Interface.Log.Models.Exception>();
+            config.CreateMap<Metric, IMetric>();
+            config.CreateMap<IMetric, Metric>();
+            config.CreateMap<Trace, ITrace>();
+            config.CreateMap<ITrace, Trace>();
+            config.CreateMap<PurgeWorker, IPurgeWorker>()
+            .ForMember(pw => pw.Status, options => options.MapFrom(pw => (PurgeWorkerStatus)(pw.Status ?? (short)PurgeWorkerStatus.Ready)))
+            ;
+            config.CreateMap<IPurgeWorker, PurgeWorker>()
+            .ForMember(pw => pw.Status, options => options.MapFrom(pw => (short)pw.Status))
+            ;
+        }
+
+        public Mapper Create() => new Mapper(_mapperConfiguratin);
+    }
+}
