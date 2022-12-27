18bc1e76ee65fbe061c1457ba55076b285fb3813
Greg Vondra <gvondra@gmail.com>
2022-12-27 15:41:06 -0600

    update add authentication call

--- Log/LogAPI/Program.cs
+++ Log/LogAPI/Program.cs
@@ -1,6 +1,7 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
-using LogAPI;
+using BrassLoon.CommonAPI;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,74 +9,88 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Serialization;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-builder.Host.ConfigureContainer((ContainerBuilder builder) => builder.RegisterModule(new LogAPIModule()));
-// Add services to the container.
-builder.Services.Configure<LogAPI.Settings>(builder.Configuration);
-builder.Services.AddControllers()
-    .AddNewtonsoftJson(o =>
-    {
-        o.SerializerSettings.ContractResolver = new DefaultContractResolver();
-    })
-    .AddJsonOptions(o =>
-    {
-        o.JsonSerializerOptions.PropertyNamingPolicy = null;
-    })
-    ;
-builder.Services.AddCors(builder.Configuration);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(o =>
+namespace LogAPI
 {
-    o.SwaggerDoc(
-        "v1",
-        new OpenApiInfo
-        {
-            Version = "v1",
-            Title = "Brass Loon Account API"
-        });
-    o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    public class Program
     {
-        In = ParameterLocation.Header,
-        Description = "Please insert JWT with Bearer into field",
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey
-    });
-    o.AddSecurityRequirement(new OpenApiSecurityRequirement {
-    {
-        new OpenApiSecurityScheme
+        public static void Main(string[] args)
         {
-        Reference = new OpenApiReference
-        {
-            Type = ReferenceType.SecurityScheme,
-            Id = "Bearer"
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+            builder.Host.ConfigureContainer((ContainerBuilder builder) => builder.RegisterModule(new LogAPIModule()));
+            // Add services to the container.
+            builder.Services.Configure<Settings>(builder.Configuration);
+            builder.Services.AddControllers()
+                .AddNewtonsoftJson(o =>
+                {
+                    o.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                })
+                .AddJsonOptions(o =>
+                {
+                    o.JsonSerializerOptions.PropertyNamingPolicy = null;
+                })
+                ;
+            builder.Services.AddCors(builder.Configuration);
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(o =>
+            {
+                o.SwaggerDoc(
+                    "v1",
+                    new OpenApiInfo
+                    {
+                        Version = "v1",
+                        Title = "Brass Loon Account API"
+                    });
+                o.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                o.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                    },
+                    new string[] { }
+                    }
+                });
+            });
+            builder.Services.AddAuthentication(o =>
+            {
+                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddBrassLoonAuthentication(builder.Configuration)
+            ;
+            builder.Services.AddSingleton<IAuthorizationHandler, AuthorizationHandler>();
+            builder.Services.AddAuthorization(builder.Configuration);
+
+            WebApplication app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseRouting();
+            app.UseCors();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+            app.Run();
         }
-        },
-        new string[] { }
-        } 
-    });
-});
-builder.Services.AddAuthentication(builder.Configuration);
-builder.Services.AddSingleton<IAuthorizationHandler, AuthorizationHandler>();
-builder.Services.AddAuthorization(builder.Configuration);
-
-WebApplication app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    }
 }
-
-app.UseRouting();
-app.UseCors();
-app.UseAuthentication();
-app.UseAuthorization();
-
-//app.MapControllers();
-app.UseEndpoints(endpoints => endpoints.MapControllers());
-
-app.Run();
