492a6a0a6f7bbe5df577786baeb7221de21ab0c8
Greg Vondra <gvondra@gmail.com>
2022-12-27 15:40:18 -0600

    update settings classes to work with CommonAPI

--- Log/LogAPI/Settings.cs
+++ Log/LogAPI/Settings.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BrassLoon.CommonAPI;
 
 namespace LogAPI
 {
-    public class Settings
-    {
-        public string ConnectionString { get; set; }
-        public string ConnectionStringUser { get; set; }
-        public bool EnableDatabaseAccessToken { get; set; } = false;
-        public string KeyVaultAddress { get; set; }
-        public string AccountApiBaseAddress { get; set; }
-        public string LogApiBaseAddress { get; set; }
-        public string ExceptionLoggingDomainId { get; set; }
-    }
+    public class Settings : CommonApiSettings { }
 }
