2a9b9126b6a88c1b3bbe0aef7f1c0092d470cc05
Greg Vondra <gvondra@gmail.com>
2023-01-02 07:51:21 -0600

    update attribute

--- Log/Interface.Log/IExceptionService.cs
+++ Log/Interface.Log/IExceptionService.cs
@@ -15,7 +15,6 @@ namespace BrassLoon.Interface.Log
             string category = null, 
             string level = null,
             LogModels.EventId? eventId = null);
-        [Obsolete()]
         Task<LogModels.Exception> Create(ISettings settings, Guid domainId, System.Exception exception);
         [Obsolete()]
         Task<LogModels.Exception> Create(ISettings settings, Guid domainId, DateTime? createTimestamp, System.Exception exception);
