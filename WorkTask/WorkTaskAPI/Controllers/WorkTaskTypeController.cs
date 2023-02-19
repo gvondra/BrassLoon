abba1a767343a5f270f1ce812c9b56b463203af0
Greg Vondra <gvondra@gmail.com>
2023-02-19 07:55:07 -0600

    add save methods

--- WorkTask/WorkTaskAPI/Controllers/WorkTaskTypeController.cs
+++ WorkTask/WorkTaskAPI/Controllers/WorkTaskTypeController.cs
@@ -64,5 +64,91 @@ namespace WorkTaskAPI.Controllers
             }
             return result;
         }
+
+        [NonAction]
+        private IActionResult ValidateRequest(WorkTaskType workTaskType)
+        {
+            IActionResult result = null;
+            if (result == null && workTaskType == null)
+                result = BadRequest("Missing work task type body");
+            if (result == null && string.IsNullOrEmpty(workTaskType?.Title))
+                result = BadRequest("Missing work task type title value");
+            return result;
+        }
+
+        [HttpPost("{domainId}")]
+        [Authorize(Constants.POLICY_BL_AUTH)]
+        [ProducesResponseType(typeof(WorkTaskType), 200)]
+        public async Task<IActionResult> Create([FromRoute] Guid? domainId, [FromBody] WorkTaskType workTaskType)
+        {
+            IActionResult result = null;
+            try
+            {
+                if (result == null && (!domainId.HasValue || domainId.Value.Equals(Guid.Empty)))
+                    result = BadRequest("Missing domain id parameter value");
+                if (result == null && !(await VerifyDomainAccount(domainId.Value)))
+                    result = StatusCode(StatusCodes.Status401Unauthorized);
+                if (result == null)
+                    result = ValidateRequest(workTaskType);
+                if (result == null)
+                {
+                    CoreSettings settings = CreateCoreSettings();
+                    IMapper mapper = CreateMapper();
+                    IWorkTaskType innerWorkTaskType = _workTaskTypeFactory.Create(domainId.Value);
+                    mapper.Map(workTaskType, innerWorkTaskType);
+                    await _workTaskTypeSaver.Create(settings, innerWorkTaskType);
+                    result = Ok(
+                        mapper.Map<WorkTaskType>(innerWorkTaskType)
+                        );
+                }
+            }
+            catch (Exception ex)
+            {
+                await LogException(ex);
+                result = StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            return result;
+        }
+
+        [HttpPut("{domainId}/{id}")]
+        [Authorize(Constants.POLICY_BL_AUTH)]
+        [ProducesResponseType(typeof(WorkTaskType), 200)]
+        public async Task<IActionResult> Save([FromRoute] Guid? domainId, [FromRoute] Guid? id, [FromBody] WorkTaskType workTaskType)
+        {
+            IActionResult result = null;
+            try
+            {
+                if (result == null && (!id.HasValue || id.Value.Equals(Guid.Empty)))
+                    result = BadRequest("Missing id parameter value");
+                if (result == null && (!domainId.HasValue || domainId.Value.Equals(Guid.Empty)))
+                    result = BadRequest("Missing domain id parameter value");
+                if (result == null && !(await VerifyDomainAccount(domainId.Value)))
+                    result = StatusCode(StatusCodes.Status401Unauthorized);
+                if (result == null)
+                    result = ValidateRequest(workTaskType);
+                if (result == null)
+                {
+                    CoreSettings settings = CreateCoreSettings();
+                    IWorkTaskType innerWorkTaskType = await _workTaskTypeFactory.Get(settings, id.Value);
+                    if (innerWorkTaskType == null)
+                        result = NotFound();
+                    if (result == null)
+                    {
+                        IMapper mapper = CreateMapper();
+                        mapper.Map(workTaskType, innerWorkTaskType);
+                        await _workTaskTypeSaver.Update(settings, innerWorkTaskType);
+                        result = Ok(
+                            mapper.Map<WorkTaskType>(innerWorkTaskType)
+                            );
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await LogException(ex);
+                result = StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            return result;
+        }
     }
 }
