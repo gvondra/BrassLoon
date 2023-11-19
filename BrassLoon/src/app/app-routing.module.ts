36ff49a7db357cd6390dc57707dc61169bb1a962
Greg Vondra <gvondra@gmail.com>
2023-11-19 10:03:37 -0600

    upgrade to angular 15

--- BrassLoon/src/app/app-routing.module.ts
+++ BrassLoon/src/app/app-routing.module.ts
@@ -197,7 +197,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, {})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
