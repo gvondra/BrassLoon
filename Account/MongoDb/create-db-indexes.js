b09dcb77527aa1203749ea3244c5790f188e0737
Greg Vondra <greg.vondra@outlook.com>
2024-12-15 10:10:48 -0600

    uncomment

--- Account/MongoDb/create-db-indexes.js
+++ Account/MongoDb/create-db-indexes.js
@@ -12,20 +12,20 @@ function CreateIndex(collection, keys, options) {
     }
 }
 db = db.getSiblingDB("dev-bl-account");
-// // AccountUser
-// CreateIndex(db.AccountUser, { "AccountGuid": 1, "UserGuid": 1 }, { "name": "IX_AccountUser_AccountGuid_UserGuid", "unique": true  });
-// CreateIndex(db.AccountUser, { "AccountGuid": 1 }, { "name": "IX_AccountUser_AccountGuid"  });
-// CreateIndex(db.AccountUser, { "UserGuid": 1 }, { "name": "IX_AccountUser_UserGuid"  });
-// // Client
-// CreateIndex(db.Client, { "AccountGuid": 1 }, { "name": "IX_Client_AccountGuid" });
-// // ClientCredential
+// AccountUser
+CreateIndex(db.AccountUser, { "AccountGuid": 1, "UserGuid": 1 }, { "name": "IX_AccountUser_AccountGuid_UserGuid", "unique": true  });
+CreateIndex(db.AccountUser, { "AccountGuid": 1 }, { "name": "IX_AccountUser_AccountGuid"  });
+CreateIndex(db.AccountUser, { "UserGuid": 1 }, { "name": "IX_AccountUser_UserGuid"  });
+// Client
+CreateIndex(db.Client, { "AccountGuid": 1 }, { "name": "IX_Client_AccountGuid" });
+// ClientCredential
 // CreateIndex(db.ClientCredential, { "ClientId": 1 }, { "name": "IX_ClientCredential_ClientId" });
-// // Domain
-// CreateIndex(db.Domain, { "AccountGuid": 1 }, { "name": "IX_Domain_AccountGuid" });
-// // EmailAddress
-// CreateIndex(db.EmailAddress, { "Address": 1 }, { "name": "IX_EmailAddress_Address", "unique": true });
-// // User
-// CreateIndex(db.User, { "ReferenceId": 1 }, { "name": "IX_User_ReferenceId", "unique": true });
-// CreateIndex(db.User, { "EmailAddressGuid": 1 }, { "name": "IX_User_EmailAddressGuid" });
-// // UserInvitation
+// Domain
+CreateIndex(db.Domain, { "AccountGuid": 1 }, { "name": "IX_Domain_AccountGuid" });
+// EmailAddress
+CreateIndex(db.EmailAddress, { "Address": 1 }, { "name": "IX_EmailAddress_Address", "unique": true });
+// User
+CreateIndex(db.User, { "ReferenceId": 1 }, { "name": "IX_User_ReferenceId", "unique": true });
+CreateIndex(db.User, { "EmailAddressGuid": 1 }, { "name": "IX_User_EmailAddressGuid" });
+// UserInvitation
 // CreateIndex(db.UserInvitation, { "AccountGuid": 1 }, { "name": "IX_UserInvitation_AccountGuid" });
