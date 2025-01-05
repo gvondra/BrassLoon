194ad8ede53dc3c25c0d6eccdad6361b60700cf4
Greg Vondra <greg.vondra@outlook.com>
2025-01-05 11:30:53 -0600

    pass use mongo db indicator to dependency registration

--- Address/AddressRPC/AddressRPCModule.cs
+++ Address/AddressRPC/AddressRPCModule.cs
@@ -8,11 +8,18 @@ namespace AddressRPC
 #pragma warning disable S101 // Types should be named in PascalCase
     public class AddressRPCModule : Module
     {
+        private readonly bool _useMongoDb;
+
+        public AddressRPCModule(bool useMongoDb)
+        {
+            _useMongoDb = useMongoDb;
+        }
+
         protected override void Load(ContainerBuilder builder)
         {
             base.Load(builder);
             _ = builder.RegisterModule(new BrassLoon.Interface.Account.AccountInterfaceModule());
-            _ = builder.RegisterModule(new AddressCoreModule());
+            _ = builder.RegisterModule(new AddressCoreModule(_useMongoDb));
             _ = builder.RegisterType<AddressService>();
             _ = builder.RegisterType<DomainAcountAccessVerifier>().As<IDomainAcountAccessVerifier>();
             _ = builder.RegisterType<EmailAddressService>();
--- Address/AddressRPC/Program.cs
+++ Address/AddressRPC/Program.cs
@@ -18,8 +18,9 @@ namespace AddressRPC
         public static void Main(string[] args)
         {
             WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            string useMongoDb = builder.Configuration["UseMongoDb"] ?? "false";
             _ = builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-            _ = builder.Host.ConfigureContainer((ContainerBuilder builder) => builder.RegisterModule(new AddressRPCModule()));
+            _ = builder.Host.ConfigureContainer((ContainerBuilder builder) => builder.RegisterModule(new AddressRPCModule(bool.Parse(useMongoDb))));
             _ = builder.Services.Configure<Settings>(builder.Configuration);
 
             _ = builder.Services.AddLogging(b =>
