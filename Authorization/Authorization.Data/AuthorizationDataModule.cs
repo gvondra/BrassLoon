e753fa33432864f43092bbff4d6dbb2296fd7744
Greg Vondra <greg.vondra@outlook.com>
2025-01-11 17:14:58 -0600

    add email address data factory and saver

--- Authorization/Authorization.Data/AuthorizationDataModule.cs
+++ Authorization/Authorization.Data/AuthorizationDataModule.cs
@@ -52,6 +52,8 @@ namespace BrassLoon.Authorization.Data
             _ = builder.RegisterType<DbProvider>().As<IDbProvider>();
             _ = builder.RegisterType<InternalMongoDb.ClientDataFactory>().As<IClientDataFactory>();
             _ = builder.RegisterType<InternalMongoDb.ClientDataSaver>().As<IClientDataSaver>();
+            _ = builder.RegisterType<InternalMongoDb.EmailAddressDataFactory>().As<IEmailAddressDataFactory>();
+            _ = builder.RegisterType<InternalMongoDb.EmailAddressDataSaver>().As<IEmailAddressDataSaver>();
             _ = builder.RegisterType<InternalMongoDb.UserDataFactory>().As<IUserDataFactory>();
             _ = builder.RegisterType<InternalMongoDb.UserDataSaver>().As<IUserDataSaver>();
             // the following BsonClassMap are out of place. Just threw it here for simplicity
---
+++ Authorization/Authorization.Data/Internal/MongoDb/EmailAddressDataFactory.cs
@@ -0,0 +1,32 @@
+﻿using BrassLoon.Authorization.Data.Models;
+using BrassLoon.DataClient.MongoDB;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+
+namespace BrassLoon.Authorization.Data.Internal.MongoDb
+{
+    public class EmailAddressDataFactory : IEmailAddressDataFactory
+    {
+        private readonly IDbProvider _dbProvider;
+
+        public EmailAddressDataFactory(IDbProvider dbProvider)
+        {
+            _dbProvider = dbProvider;
+        }
+
+        public async Task<EmailAddressData> Get(CommonData.ISettings settings, Guid id)
+        {
+            IMongoCollection<EmailAddressData> collection = await _dbProvider.GetCollection<EmailAddressData>(settings, Constants.CollectionName.EmailAddress);
+            FilterDefinition<EmailAddressData> filter = Builders<EmailAddressData>.Filter.Eq(em => em.EmailAddressId, id);
+            return await collection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task<EmailAddressData> GetByAddressHash(CommonData.ISettings settings, byte[] hash)
+        {
+            IMongoCollection<EmailAddressData> collection = await _dbProvider.GetCollection<EmailAddressData>(settings, Constants.CollectionName.EmailAddress);
+            FilterDefinition<EmailAddressData> filter = Builders<EmailAddressData>.Filter.Eq(em => em.AddressHash, hash);
+            return await collection.Find(filter).FirstOrDefaultAsync();
+        }
+    }
+}
