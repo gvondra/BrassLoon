52032caab7a65f994b6a3d7a44d478112c4c3e5f
Greg Vondra <greg.vondra@outlook.com>
2025-02-01 10:19:28 -0600

    add id property

--- WorkTask/WorkTask.Data/Models/WorkTaskTypeGroupData.cs
+++ WorkTask/WorkTask.Data/Models/WorkTaskTypeGroupData.cs
@@ -5,6 +5,10 @@ namespace BrassLoon.WorkTask.Data.Models
 {
     public class WorkTaskTypeGroupData : DataManagedStateBase
     {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
         [ColumnMapping(IsPrimaryKey = true)]
         [BsonGuidRepresentation(MongoDB.Bson.GuidRepresentation.Standard)]
         public Guid WorkTaskTypeId { get; set; }
